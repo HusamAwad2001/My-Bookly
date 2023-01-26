a5c5ef50ca12b3905c5fa6bbc6c8f6531dc20971
Husam Dahliz <www.hosamawad2016@gmail.com>
2023-01-26 10:15:10 +0200

    Create Search Screen

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -3,10 +3,12 @@ import 'package:my_bookly/features/home/presentation/views/home_screen.dart';
 
 import '../../features/home/presentation/views/book_details_screen.dart';
 import '../../features/launch/presentation/views/launch_screen.dart';
+import '../../features/search/presentation/views/search_screen.dart';
 
 abstract class AppRouter{
   static const kHomeScreen = '/home_screen';
   static const kBookDetailsScreen = '/book_details_screen';
+  static const kSearchScreen = '/search_screen';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -20,6 +22,10 @@ abstract class AppRouter{
       GoRoute(
         path: kBookDetailsScreen,
         builder: (context, state) => const BookDetailsScreen(),
+      ),
+      GoRoute(
+        path: kSearchScreen,
+        builder: (context, state) => const SearchScreen(),
       ),
     ],
   );
---
+++ lib/features/search/presentation/views/search_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:my_bookly/features/search/presentation/views/widgets/search_screen_body.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: SearchScreenBody(),
+      ),
+    );
+  }
+}
