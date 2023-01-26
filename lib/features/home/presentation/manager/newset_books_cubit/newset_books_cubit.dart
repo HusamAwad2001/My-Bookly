7b62d623a54e7be805865b434f6215e881b19d8e
Husam Dahliz <www.hosamawad2016@gmail.com>
2023-01-26 19:05:29 +0200

    Newset books cubit done

--- lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -2,9 +2,22 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 import '../../../data/models/book_model/book_model.dart';
+import '../../../data/repos/home_repo.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async{
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.errorMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
 }
