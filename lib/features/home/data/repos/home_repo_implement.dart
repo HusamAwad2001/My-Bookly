093e43dbce3b5b8043f154436ea76a7d31d4639b
Husam Dahliz <www.hosamawad2016@gmail.com>
2023-01-26 18:39:33 +0200

    Create Server failure from dio error

--- lib/features/home/data/repos/home_repo_implement.dart
+++ lib/features/home/data/repos/home_repo_implement.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:my_bookly/core/errors/failures.dart';
 import 'package:my_bookly/features/home/data/models/book_model/book_model.dart';
 import '../../../../core/utils/api_service.dart';
@@ -14,14 +15,17 @@ class HomeRepoImplement implements HomeRepo {
     try {
       var data = await apiService.get(
           endPoint:
-          'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming');
+              'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming');
       List<BookModel> books = [];
-      for(var item in data['items']) {
+      for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
       }
       return right(books);
-    }catch (error) {
-      return left(ServerFailure());
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
