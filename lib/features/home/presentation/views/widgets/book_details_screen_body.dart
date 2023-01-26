8b5e6f65454988e99c5c307bf0a029568404ba15
Husam Dahliz <www.hosamawad2016@gmail.com>
2023-01-26 09:56:22 +0200

    ADD custom scroll view

--- lib/features/home/presentation/views/widgets/book_details_screen_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_screen_body.dart
@@ -14,6 +14,63 @@ class BookDetailsScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: p24),
+            child: Column(
+              children: [
+                const CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .2),
+                  child: const CustomBookImage(),
+                ),
+                const SizedBox(height: 43),
+                Text(
+                  'The Jungle Book',
+                  style: Styles.textStyle30.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 6),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    'RudYard Kipling',
+                    style: Styles.textStyle18.copyWith(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 18),
+                const BookRating(mainAxisAlignment: MainAxisAlignment.center),
+                const SizedBox(height: 37),
+                const BooksAction(),
+                const Expanded(
+                  child: SizedBox(height: 50),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle14.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 14),
+                const SimilarBooksListView(),
+                const SizedBox(height: 25),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: p24),
       child: Column(
@@ -45,7 +102,9 @@ class BookDetailsScreenBody extends StatelessWidget {
           const BookRating(mainAxisAlignment: MainAxisAlignment.center),
           const SizedBox(height: 37),
           const BooksAction(),
-          const SizedBox(height: 50),
+          const Expanded(
+            child: SizedBox(height: 50),
+          ),
           Align(
             alignment: Alignment.centerLeft,
             child: Text(
@@ -57,6 +116,7 @@ class BookDetailsScreenBody extends StatelessWidget {
           ),
           const SizedBox(height: 14),
           const SimilarBooksListView(),
+          const SizedBox(height: 25),
         ],
       ),
     );
